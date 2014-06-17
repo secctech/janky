68908332e311749c44af5bd88b4467f87de2a239
Neil Mock <neilmock@gmail.com>
2014-06-17 18:57:49 -0500

    run the deploy in a sep thread.  this could be dangerous in ruby

--- Gemfile
+++ Gemfile
@@ -2,6 +2,6 @@ source "http://rubygems.org"
 
 gem "hipchat", "~>0.4"
 gem "pg",      "0.17.1"
-gem 'thin',    "1.6.2"
+gem "thin",    "1.6.2"
 
 gemspec
--- lib/janky/notifier/deploy.rb
+++ lib/janky/notifier/deploy.rb
@@ -6,14 +6,12 @@ module Janky
         success = build.green?
         branch  = build.branch_name
 
-        puts "Processing #{build.repo_name}..."
-
         if branch == "staging"
-          puts "Cloning API..."
-
-          `git clone git@github.com:secctech/api.git tmp/api`
-          `cd tmp/api && git checkout -b staging remotes/origin/staging`
-          `cd tmp/api && git push -f git@heroku.com:secondchance-api-staging.git HEAD:master`
+          Thread.new do
+            `git clone git@github.com:secctech/api.git tmp/api`
+            `cd tmp/api && git checkout -b staging remotes/origin/staging`
+            `cd tmp/api && git push -f git@heroku.com:secondchance-api-staging.git HEAD:master`
+          end
         end
       end
     end
